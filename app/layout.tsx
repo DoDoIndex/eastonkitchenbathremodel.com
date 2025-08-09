82c3c5f072413b349cbab5e600f4f164e7b39ef7
An Do <ado008@ucr.edu>
2025-08-09 13:52:24 -0700

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Noto_Serif, Noto_Sans, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Suspense } from 'react';
 import { Toaster } from 'react-hot-toast';
+import Script from 'next/script';
 
 const notoSans = Noto_Sans({
   variable: '--font-noto-sans',
@@ -34,31 +35,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TQ6T4324');
-            `,
-          }}
+       <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=GTM-TQ6T4324`}
+          strategy="afterInteractive"
         />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'GTM-TQ6T4324');
+          `}
+        </Script>
       </head>
       <body
         className={`${notoSans.variable} ${geistMono.variable} ${notoSerif.variable} antialiased`}
       >
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TQ6T4324"
-            height="0" 
-            width="0" 
-            style={{display:'none',visibility:'hidden'}}
-          />
-        </noscript>
         {children}
         <Toaster position="bottom-right" />
       </body>
